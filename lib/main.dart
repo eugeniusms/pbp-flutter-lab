53e3ae938673c56a11194a226b4e47225bd98a14
eugeniusms <eugeniusms@gmail.com>
2022-11-06 14:24:22 +0700

    🔧 Building a Widget Tree

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ void main() => runApp(CounterSeven()); // Menjalankan aplikasi
 
 class CounterSeven extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello, World!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Program Counter"),
+        ),
+        body: Text("This is my default text!"),
+      ),
+    );
   }
 }

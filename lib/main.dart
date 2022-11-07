bb7e35f67b7c573c30e8bd7ec6cca22bedc4c35b
eugeniusms <eugeniusms@gmail.com>
2022-11-07 17:33:37 +0700

    🔨 Add Documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
-import 'package:flutter/material.dart';
-import 'pages/CounterPage.dart';
+import 'package:flutter/material.dart'; // import ui material dari flutter
+import 'pages/CounterPage.dart'; // import CounterPage widget
 
+// fungsi utama run program
 void main() {
+  // menjalankan aplikasi counter seven
   runApp(const CounterSeven());
 }
 
+// aplikasi counter seven 
 class CounterSeven extends StatelessWidget {
+  // inisiasi key pada contructor CounterSeven
   const CounterSeven({super.key});
 
   // widget root of application.
   @override
   Widget build(BuildContext context) {
+    // seperti meta tag pada html
     return MaterialApp(
       title: 'Counter 7',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CounterPage(title: 'Program Counter'),
+      // menampilkan judul pada CounterPage
+      home: const CounterPage(title: 'Program Counter'), 
     );
   }
 }
--- lib/pages/CounterPage.dart
+++ lib/pages/CounterPage.dart
@@ -1,14 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // import ui material dari flutter
 
-class CounterPage extends StatefulWidget {
+// CounterPage is a statefulwidget
+class CounterPage extends StatefulWidget { 
+  // inisiasi constructor sesuai key & title
   const CounterPage({super.key, required this.title});
-
   final String title;
 
+  // inisiasi state dari CounterPage -> _CounterState (private)
   @override
   State<CounterPage> createState() => _CounterState();
 }
 
+// _CounterState is a state of CounterPage
 class _CounterState extends State<CounterPage> {
   // variabel counter bernilai private
   int _counter = 0;
@@ -30,25 +33,29 @@ class _CounterState extends State<CounterPage> {
     });
   }
 
+  // Susunan widget dari CounterPage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar berisi judul dengan align left 
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: false,
       ),
+      // body center disusun dengan isi kolom dari child
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
 
+          // children berisi 3 widget berisi tulisan GANJIL/GENAP & updated _counter
           children: <Widget>[
             // menampilkan ganjil vs genap sesuai kondisi
             _counter % 2 == 0 
                   // saat habis dibagi 0
                   ? const Text(
-                      "GENAP", 
+                      "GENAP", // teks yang ditampilkan "GENAP"
                       style: TextStyle(
-                        color: Colors.red,
+                        color: Colors.red, // dengan warna merah
                       ),
                     )
                   // saat tidak habis dibagi 0
@@ -61,6 +68,7 @@ class _CounterState extends State<CounterPage> {
               Text(
                 // menampilkan nilai _counter
                 '$_counter',
+                // style dari teks _counter
                 style: Theme.of(context).textTheme.headline4,
               ),
           ],
@@ -68,19 +76,23 @@ class _CounterState extends State<CounterPage> {
         ),
       ),
       
-      // menambahkan tombol increment dan decrement
+      // tombol increment dan decrement
       floatingActionButton: Container(
+          // layout container button
           width: double.infinity,
           margin: const EdgeInsets.only(left:35, bottom: 5),
+
+          // the buttons
           child: Row(
+            // align antar children (kedua button)
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               // decrement button
               _counter > 0 
                 ? FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
+                  onPressed: _decrementCounter, // ketika tombol diklik maka akan setState(_counter--)
+                  tooltip: 'Decrement', // tooltip
+                  child: const Icon(Icons.remove), // icon tombol
                 ) 
                 : const SizedBox(), // jika _counter <= 0 maka tidak menampilkan tombol decrement
               // increment button

6f05e41597726553478dde4976f4e116c83ddead
eugeniusms <eugeniusms@gmail.com>
2022-11-23 12:11:47 +0700

    🔧 Remove Debug

--- lib/utils/fetch.dart
+++ lib/utils/fetch.dart
@@ -3,19 +3,16 @@ import 'dart:convert';
 import 'package:counter_7/models/watchlist.dart';
 
 Future<List<Watchlist>> fetchWatchlist() async {
-  print("MASUK");
   var url = Uri.parse(
       'https://pbp-assignment-eugeniusms.herokuapp.com/mywatchlist/json/');
-  print("MASUK 2");
   var response = await http.get(url, headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   });
-  print("MASUK 3");
   // print(response.body);
   // melakukan decode response menjadi bentuk json
   var data = jsonDecode(utf8.decode(response.bodyBytes));
-  print(data);
+  // print(data);
 
   // melakukan konversi data json menjadi object ToDo
   List<Watchlist> listWatchlist = [];
